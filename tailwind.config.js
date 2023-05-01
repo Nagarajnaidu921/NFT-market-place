92850fa3ea4d9af2f1b5f5ae11bdc5fc324b5918
Nagaraj Vemula <nagarajnaidu921@gmail.com>
2023-05-01 20:15:57 +0530

    added daisy theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +15,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["cupcake", "dark", "cmyk", "corporate"],
+  },
+  plugins: [require('daisyui')],
 }
