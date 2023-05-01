169e395df3a931ac1fff997cbcdb7c1441029fa7
Nagaraj Vemula <nagarajnaidu921@gmail.com>
2023-05-01 20:16:16 +0530

    mapped envs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
+  env: {
+    IPFS_API_KEY: process.env.IPFS_API_KEY,
+    IPFS_API_SECRET: process.env.IPFS_API_SECRET
+  }
 }
 
 module.exports = nextConfig
